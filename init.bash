bfdf0f94a83e0ce02ea3e4401e59d17bee7f9e72
Linus <93053722+KoenigLinus@users.noreply.github.com>
2024-11-21 17:27:45 +0100

    exec

--- init.bash
+++ init.bash
@@ -1,4 +1,11 @@
+rm -rf ~/.config/nvim
 ln -s ~/dotfiles/nvim ~/.config/nvim
+
+rm -rf ~/.config/weechat
 ln -s ~/dotfiles/weechat/ ~/.config/weechat
+
+rm -rf ~/.sources
 ln -s ~/dotfiles/.sources/ ~/.sources
 
+rm -rf ~/.scripts
+ln -s ~/dotfiles/.scripts/ ~/.scripts
